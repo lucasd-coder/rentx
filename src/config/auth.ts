b7b5acc03211413253f0c51d67d2f753944e2c5e
Lucas da Silva <lucasdasilva68@yahoo.com>
2021-05-23 17:36:10 -0300

    add profileUser, MailProvider, StorageProvider, config mail, production config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
-  secret_token: 'd3eb94d4d0b0695202b3ba20228af7f7',
-  expires_in_token: '15m',
-  secret_refresh_token: '429908b7d7a8053c1eed96a828efd59d',
-  expires_in_refresh_token: '30d',
+  secret_token: process.env.SECRET_TOKEN,
+  expires_in_token: process.env.EXPIRES_IN_TOKEN,
+  secret_refresh_token: process.env.SECRET_REFRESH_TOKEN,
+  expires_in_refresh_token: process.env.EXPIRES_IN_REFRESH_TOKEN,
   expires_refresh_token_days: 30,
 };
--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  refresh_token: string;
+  token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
     @inject('DayjsDateProvider')
     private dayjsDateProvider: IDateProvider,
   ) {}
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { sub, email } = verify(token, auth.secret_refresh_token) as IPayload;
 
     const user_id = sub;
@@ -50,7 +55,15 @@ class RefreshTokenUseCase {
       user_id,
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return {
+      refresh_token,
+      token: newToken,
+    };
   }
 }
 
